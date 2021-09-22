4a4654d169f5f08088edf4c7c67b00db5179b3af
tikamaisarah <tikamaisarah43483@gmail.com>
2021-09-22 09:10:36 +0700

    Perubahan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -446,7 +446,7 @@
                                 <path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
                                 <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
                             </svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Laracasts</a></div>
+                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Tika Gemes</a></div>
                         </div>
 
                         <div class="ml-12">
